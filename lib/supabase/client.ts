e8aaf7f5cbe93598d640db3631de43d336010024
why459097630 <whywhy9264@gmail.com>
2026-04-12 22:00:48 +0800

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
   return createBrowserClient(
-    process.env.WEB_SUPABASE_URL!,
-    process.env.WEB_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.NEXT_PUBLIC_WEB_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_WEB_SUPABASE_PUBLISHABLE_KEY!,
   );
 }
