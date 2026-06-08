219a6cda366f107a6e154d7342e4227546fc8a9b
why459097630 <whywhy9264@gmail.com>
2026-06-08 22:18:30 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@resvg/resvg-js"],
+  outputFileTracingIncludes: {
+    "/api/download-pwa-package": ["./assets/fonts/*.ttf"],
+    "/api/dev-preview-qr-poster": ["./assets/fonts/*.ttf"],
+  },
 };
 
 export default nextConfig;
